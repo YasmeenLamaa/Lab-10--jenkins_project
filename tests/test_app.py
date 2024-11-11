fd8f3b5a6e202962a5209871a2a4a1739190dcf7
YasmeenLamaa <ytl00@mail.aub.edu>
2024-11-11 15:05:49 +0200

    updated test file to locate root directory

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,7 +2,7 @@ import unittest
 from app import greet
 import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '.')))
 
 class TestApp(unittest.TestCase):
     def test_greet(self):
